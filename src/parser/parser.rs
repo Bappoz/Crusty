f9f6c98a4e267b68e8732b185212b5b6991fb0e9
Hugo Freitas Silva <videogame041@gmail.com>
2026-04-21 01:41:52 -0300

    feat(parser): add parser struct and precedence enum

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,3 +1,14 @@
 //core do parser
 //controle de fluxo, orquestrador
 // parse_expression(precedence)
+//guardar tokens
+//controlar índice atual
+//consumir tokens
+//iniciar parsing
+//chamar rules
+
+pub struct Parser {
+    tokens: Vec<Token>,
+    current: usize,
+    diagnostics: Vec<CompilerError>,
+}
