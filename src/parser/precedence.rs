e70d613507760b27495418301841c6e31b68d032
Hugo Freitas Silva <videogame041@gmail.com>
2026-04-21 01:41:52 -0300

    feat(parser): add parser struct and precedence enum

--- src/parser/precedence.rs
+++ src/parser/precedence.rs
@@ -1,1 +1,17 @@
 //binding power
+
+pub enum Precedence {
+    None,
+    Assignment,
+    LogicalOr,
+    LogicalAnd,
+    Equality,
+    Comparison,
+    Term,
+    Factor,
+    Unary,
+    Call,
+    Primary,
+}
+
+pub fn precedence_of(token: &TokenKind) -> Precedence
