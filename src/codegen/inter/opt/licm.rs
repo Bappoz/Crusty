64619060f81ea9c146f115dd8f33e93a212abe6f
Philipe Caetano <philipe2015amancio@hotmail.com>
2026-06-23 13:07:49 -0300

    feat/ add function run: search back-edges

--- src/codegen/inter/opt/licm.rs
+++ src/codegen/inter/opt/licm.rs
@@ -1,5 +1,6 @@
 use super::OptPass;
-use crate::codegen::inter::Cfg;
+use crate::codegen::inter::{Cfg, BlockId};
+use crate::collections::{HashMap, HashSet};
 
 pub struct LoopInvariantCodeMotionPass;
 
@@ -9,6 +10,31 @@ impl OptPass for LoopInvariantCodeMotionPass {
     }
 
     fn run(&self, _cfg: &mut Cfg) -> bool {
-        false
+        let dominators = self.compute_dominators(cfg);
+
+        let mut mutated = false;
+        let mut back_edge = Vec::new(); // aresta que liga o bloco dominante a dominado
+
+        for &block in cfg.block.keys(){
+            if let Some(cfg_block) = cfg.blocks.get(&block){
+                for &succ in &cfg_block.succesors {
+                    if let Some(doms) = dominators.get(&block){
+                        if doms.contains(&succ){
+                            back_edges.push((succ, block));
+                        }
+                    }
+                }
+
+            }
+        }
+
+        for (header, tail) in back_edges {
+            let loop_body = self.get_loop_body(cfg, header, tail);
+
+            prtinln("Laço detectado! Header {:?}", header, tail);
+            println("Blocos pertencentes ao laço: {:?}", loop_body);
+
+            TODO:
+        }
     }
 }
